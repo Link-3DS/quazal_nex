28d3d703a3268ac0afd76a8d58452965c4fd6e3d
rraptor06 <kerallanmathis162@gmail.com>
2025-07-04 19:08:55 +0200

    Added DummyCompression + ZlibCompression.

--- common.py
+++ common.py
@@ -1,3 +1,31 @@
+import zlib
+
+
+class DummyCompression:
+    def compress(self, data): 
+        return data
+
+    def decompress(self, data): 
+        return data
+
+
+class ZlibCompression:
+    def compress(self, data):
+        compressed = zlib.compress(data)
+        ratio = int(len(data) / len(compressed) + 1)
+        return bytes([ratio]) + compressed
+
+    def decompress(self, data):
+        if data[0] == 0:
+            return data[1:]
+
+        decompressed = zlib.decompress(data[1:])
+        ratio = int(len(decompressed) / (len(data) - 1) + 1)
+        if ratio != data[0]:
+            raise ValueError("Unexpected compression ratio (expected %i, got %i)" %ratio, data[0])
+        return decompressed
+
+
 class Structure:
     pass # TODO - Finish the Structure class
 
