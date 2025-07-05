1d7ecde1c654ca65d336a96149436ec5b10f1436
rraptor06 <kerallanmathis162@gmail.com>
2025-07-05 23:46:21 +0200

    Added new functions for StreamOut and StreamIn.

--- streams.py
+++ streams.py
@@ -5,7 +5,38 @@ class StreamOut(streams.StreamOut):
     def __init__(self):
         super().__init__("<")
 
+    def string(self, string):
+        if string is None:
+            self.u16(0)
+        else:
+            data = (string + "\0").encode("utf8")
+            self.u16(len(data))
+            self.write(data)
+
+    def buffer(self, data):
+        self.u32(len(data))
+        self.write(data)
+
+    def add(self, inst):
+        inst.encode(self)
+
 
 class StreamIn(streams.StreamIn):
     def __init__(self, data):
         super().__init__(data, "<")
+
+    def string(self):
+        length = self.u16()
+        if length:
+            return self.read(length).decode("utf8")[:-1]
+
+    def buffer(self): 
+        return self.read(self.u32())
+
+    def substream(self):
+        return StreamIn(self.buffer())
+    
+    def extract(self, cls):
+        inst = cls()
+        inst.decode(self)
+        return inst
